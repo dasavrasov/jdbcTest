a9b90a8611812fb79693cc8551d6366faf21aea7
DenisS <d_savrasov@rambler.ru>
2023-03-10 18:31:17 +0300

    Часть 5: Ожидания

--- src/test/java/PikabuTests.java
+++ src/test/java/PikabuTests.java
@@ -1,12 +1,17 @@
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.sql.DriverManager;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,36 +23,59 @@ public class PikabuTests {
     @BeforeEach
     public void startup(){
         System.setProperty("webdriver.chrome.driver","D:/STEP-UP/UI/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofMillis(30000));
+
     }
+
+    //Написано кастомное ожидание текста «Полетели в Калининград» и есть проверка, что картинка действительно отобразилась верная – для пункта 3
+    public static void kalinigradImgDisplayed(WebElement element, long maxTime){
+        Awaitility.with().pollDelay(Duration.ofMillis(100)).await().atMost(Duration.ofMillis(maxTime)).until(element::isDisplayed);
+    }
+
 
     @Test
     public void test1(){
-        driver.get("https://pikabu.ru/");
+        //1. Открыть сайт google.com и ввести в строку поиска «Сайт компании Победа», после чего нажать Enter.
+        driver.get("https://www.google.com/");
 
-        assertEquals(driver.getCurrentUrl(),"https://pikabu.ru/");
-        assertEquals(driver.getTitle(),"Горячее – самые интересные и обсуждаемые посты | Пикабу");
+        assertEquals(driver.getCurrentUrl(),"https://www.google.com/");
+
         //Нажимаем кнопку "Войти"
-        driver.findElement(By.cssSelector(".header-right-menu__login-button")).click();
+        driver.findElement(By.cssSelector("input[title=\"Поиск\"]")).click();
+        driver.findElement(By.cssSelector("input[title=\"Поиск\"]")).sendKeys("Сайт компании Победа");
+        driver.findElement(By.cssSelector("input[title=\"Поиск\"]")).sendKeys((Keys.ENTER));
 
-        //4. Убедиться, что отображается модальное окно «Авторизация», отображаются поля «Логин» и «Пароль», отображается кнопка «Войти».
-        driver.findElement(By.cssSelector(".popup__wrapper")).isDisplayed();
-        driver.findElement(By.cssSelector(".auth__notice")).isDisplayed();
-        driver.findElement(By.cssSelector("div .popup__wrapper button[type=\"submit\"]")).isDisplayed();
+        //2. Дождаться прогрузки страницы с результатами поиска, после чего кликнуть на первую ссылку (https://www.pobeda.aero/).
+        driver.findElement(By.xpath("//cite[contains(.,\"https://pobeda.aero\")]")).isDisplayed();
+        driver.findElement(By.xpath("//cite[contains(.,\"https://pobeda.aero\")]")).click();
 
-        //отображаются поля «Логин» и «Пароль»
-        driver.findElement(By.cssSelector("div .popup__wrapper input[placeholder=\"Логин\"]")).isDisplayed();
-        driver.findElement(By.cssSelector("div .popup__wrapper input[placeholder=\"Пароль\"]")).isDisplayed();
+        //3. Дождаться прогрузки страницы АК «Победа», после чего дождаться появления картинки с текстом «Полетели в Калининград»
+        // и проверить, что текст на странице действительно совпадает с текстом «Полетели в Калининград»./
+        //высветилась картинка "Полетели в Калиниград"
 
-        //5. Ввести в поля данные в формате логин/пароль – Qwerty/Qwerty и нажать «Войти».
-        driver.findElement(By.cssSelector("div .popup__wrapper input[placeholder=\"Логин\"]")).sendKeys("Qwerty");
-        driver.findElement(By.cssSelector("div .popup__wrapper input[placeholder=\"Пароль\"]")).sendKeys("Qwerty");
-        driver.findElement(By.cssSelector("div .popup__wrapper button[type=\"submit\"]")).submit();
+        //Ожидание - полторы минуты
+        kalinigradImgDisplayed(driver.findElement(By.cssSelector("button img[alt*=\"kaliningrad\"]")),90000);
 
-        //6.Убедиться, что появилось сообщение об ошибке, и его текст: «Ошибка. Вы ввели неверные данные авторизации».
-        driver.findElement(By.xpath("//span[contains(.,\"Вы ввели неверные данные авторизации\")]")).isDisplayed();
-    }
+        //4. Кликнуть на переключатель языка, выбрать английский язык
+        // убедиться, что на главной странице отображаются тексты "Ticket search", "Online check-in", "Manage my booking"
+        driver.manage().window().maximize();
+        driver.findElement(By.cssSelector("button.dp-o0jqr4")).isDisplayed(); // нашли кнопку по классу
+        driver.findElement(By.cssSelector("button.dp-o0jqr4")).click();
+        driver.findElement(By.cssSelector(".dp-ic4bg9")).isDisplayed(); // нашли по классу
+        driver.findElement(By.cssSelector(".dp-ic4bg9")).click();
+
+//        выбрать английский язык
+        driver.findElement(By.cssSelector("div.dp-Popover-dropdown button:nth-child(2)")).click();
+
+//        на главной странице отображаются тексты "Ticket search", "Online check-in", "Manage my booking"
+        driver.findElement(By.xpath("//button//div[contains(.,\"Ticket search\")]")).isDisplayed();
+        driver.findElement(By.xpath("//button//div[contains(.,\"Online check-in\")]")).isDisplayed();
+        driver.findElement(By.xpath("//button//div[contains(.,\"Manage my booking\")]")).isDisplayed();
+}
 
     @AfterEach
     public void shutDown(){
